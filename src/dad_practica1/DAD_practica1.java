4dca3ea05de58fb95df349208d21fa5eb6ad2e4a
2dama <2dama@PC221020.informatica.edu>
2016-09-15 14:53:12 +0100

    Añadido cambio 1

---
+++ src/dad_practica1/DAD_practica1.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ * 
+ * 
+ */
+package dad_practica1;
+
+/**
+ *
+ * @author Romen Díaz Padrón
+ */
+public class DAD_practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo");
+        System.out.println("cambio 1");
+    }
+    
+}
